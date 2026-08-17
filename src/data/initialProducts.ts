eddbd6109af5ac2f5c59ed9cc6b582d786668b5d
forsueloadrian21-cloud <forsueloadrian21@gmail.com>
2026-08-16 20:40:53 -0700

    Product.tsx , InitialProduct.ts
    nag butang nakog code sa productts unya sa initialProduct sab 6 to kabook akoang ge butang na product

---
+++ src/data/initialProducts.ts
@@ -0,0 +1,52 @@
+ import { Product } from '../types/Product';
+
+export const initialProducts: Product[] = [
+  {
+    id: '1',
+    name: 'UA Snapback - Pandas',
+    price: 1499,
+    category: 'Footwear',
+    image: 'https://www.uaworldwide.com/cdn/shop/files/ua-product-graphic-5e.png?v=1773587257',
+    description: 'Confidence to wear everyday',
+  },
+  {
+    id: '2',
+    name: 'UA Snapback - Triple Black',
+    price: 1499,
+    category: 'Headwear',
+    image: 'https://www.uaworldwide.com/cdn/shop/files/ua-product-graphic-7o.png?v=1773587258&',
+    description: 'You can wear everywhere',
+  },
+  {
+    id: '3',
+    name: 'HGHMNDS Center Stage Cap',
+    price: 1200,
+    category: 'Headwear',
+    image: 'https://hghmnds.ph/cdn/shop/files/O1_d1b1245a-d887-43f5-965a-a91361a5bb64.jpg?v=1786757694&',
+    description: 'Cotton Wool Acrylic Five Panel Cap',
+  },
+  {
+    id: '4',
+    name: 'King Stance Car',
+    price: 1200,
+    category: 'Headwear',
+    image: 'https://hghmnds.ph/cdn/shop/files/N1_7e4c40b8-e8f8-44c5-912f-961b2d574ea5.jpg?v=1786756705&',
+    description: 'Cotton Wool Acrylic Snapback Cap W/ Embroidery',
+  },
+  {
+    id: '5',
+    name: 'DBTK ELEMENTARY WOOL CAP - ROYAL BLUE',
+    price: 1250,
+    category: 'Headwear',
+    image: 'https://dbtkco.com/cdn/shop/files/Artboard_51_843bfdd4-24f7-45bc-9fe3-03bda5a40af8.jpg?v=',
+    description: 'Build your confidence',
+  },
+  {
+    id: '6',
+    name: 'CIPHER OFF TRAIL 5 PANEL CAP - BLUE/WHITE',
+    price: 1250,
+    category: 'Headwear',
+    image: 'https://dbtkco.com/',
+    description: 'Happy to use',
+  },
+];
